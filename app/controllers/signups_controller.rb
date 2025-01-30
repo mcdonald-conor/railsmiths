d772f4dea0cd12a11e644be4cd836580375dc260
Conor McDonald <cm@mcdonaldconor.com>
2025-01-30 18:33:12 +0000

    success page

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -3,24 +3,24 @@ class SignupsController < ApplicationController
     @signup = Signup.new(signup_params)
 
     if @signup.save
-      # Send welcome email
+      # Send welcome email in the background
       SignupMailer.welcome_email(@signup).deliver_later
-
-      respond_to do |format|
-        format.html { redirect_to success_signups_path }
-        format.turbo_stream { redirect_to success_signups_path }
-      end
+      redirect_to success_signups_path
     else
       flash.now[:alert] = @signup.errors.full_messages.to_sentence
       respond_to do |format|
-        format.html { redirect_to root_path }
-        format.turbo_stream
+        format.html { redirect_to root_path, alert: @signup.errors.full_messages.to_sentence }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update('form_response',
+            partial: 'shared/error_message'
+          )
+        end
       end
     end
   end
 
   def success
-    # The success action just needs to render the view
+    render layout: 'application'
   end
 
   def unsubscribe
